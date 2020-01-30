061bf2c059560d9f5f05ece11dc4a10dff589dbb
Mary Lang <mcl86@cornell.edu>
2020-01-30 14:22:45 -0700

    Reconfigure env

--- app/pojos/location.js
+++ app/pojos/location.js
@@ -2,14 +2,7 @@ class Location {
 
   constructor(response){
     this.location = response.results[0].formatted_address.split(',')[0] + ', ' + response.results[0].formatted_address.split(',')[1]
-    // this.location = location.split(',')[0].capitalize + ', ' + location.split(',')[0].upcase
   }
 }
 
 module.exports = Location;
-
-
-// const location = (location) =>
-//   ({
-//
-//   })
--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 // Update with your config settings.
+require('dotenv').config('/.env')
 
 module.exports = {
 
@@ -27,7 +28,7 @@ module.exports = {
   // add heroku login information
   production: {
     client: 'pg',
-    connection: 'postgres://mgabinfbsqxyvu:45aff5b61138f387a6e43b5bd6cd33e67d42837a163506dcf6c1ead79d337bfa@ec2-3-220-86-239.compute-1.amazonaws.com:5432/dfde5nqbce1jm2',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
       directory: './db/migrations'
     },
