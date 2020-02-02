5825be41f100de77a032a86385cdb356354fd751
Mary Lang <mcl86@cornell.edu>
2020-02-02 08:58:02 -0700

    Add user post to use bcrypt and uuid

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router  = express.Router();
-
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
@@ -14,6 +16,25 @@ router.get('/', (req, res) => {
     .catch((error) => {
       res.status(500).json({error_message: error.message});
     });
+});
+
+router.post('/', (req, res) => {
+  if (req.body.password && req.body.email) {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      database('users').insert({ email: req.body.email, password: hash, api_key: uuid.v4()})
+      .then((user) => {
+        res.status(201).json({ success: 'Account created'});
+      })
+      .catch((error) => {
+        res.status(500).json({error_message: error.message});
+      })
+      .catch((err) => {
+        res.status(400).json({error_message: err.message})
+      })
+    });
+  } else {
+    res.status(400).json({error_message: 'Email and password required'})
+  }
 });
 
 module.exports = router;
